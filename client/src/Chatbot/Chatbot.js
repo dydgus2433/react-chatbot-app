ebf62052e9642a0803a206a06670168878959205
yonghyun <dydgus2433@naver.com>
2020-12-01 08:18:41 +0900

    eventQuery and textquery in Chatbot

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,13 +1,97 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect } from "react";
 
 function Chatbot() {
+  useEffect(() => {
+    eventQuery("WelcomeToMyWebsite");
+  }, []);
+  const textQuery = async (text) => {
+    let conversations = [];
+    // First Need to take care of the  message I sent
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+    conversations.push(conversation);
+
+    //We need to take care of the message Chatbot sent
+    const textQueryVariables = {
+      text: text,
+    };
+    try {
+      // I will send request to the textQuery ROUTE
+
+      const response = await Axios.post(
+        "/api/dialogflow/textQuery",
+        textQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+
+      console.log(conversation);
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "오류가 일어난 것 같아요. 죄송해요.",
+          },
+        },
+      };
+    }
+    conversations.push(conversation);
+  };
+
+  const eventQuery = async (event) => {
+    // First Need to take care of the  message I sent
+
+    //We need to take care of the message Chatbot sent
+    const eventQueryVariables = {
+      event,
+    };
+    try {
+      // I will send request to the textQuery ROUTE
+
+      const response = await Axios.post(
+        "/api/dialogflow/eventQuery",
+        eventQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      let conversation = {
+        who: "bot",
+        content: content,
+      };
+
+      console.log(conversation);
+    } catch (error) {
+      let conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "오류가 일어난 것 같아요. 죄송해요.",
+          },
+        },
+      };
+
+      console.log(conversation);
+    }
+  };
   const keyPressHandler = (e) => {
     if (e.key === "Enter") {
       if (!e.target.value) {
         return alert("you need to type something first");
       }
       //we will send request to text query route
-      //   textQuery(e.target.value);
+      textQuery(e.target.value);
 
       e.target.value = "";
     }
